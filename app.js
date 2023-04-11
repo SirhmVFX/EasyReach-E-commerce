f65ddab1c543cfd369cd3e1e29065024dc1a94b1
sirhmvfx <sirhmvfx@gmail.com>
2023-04-11 19:09:32 +0100

    completed the routes

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express")
 const app = express()
 
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.use(express.static("public"))
+app.set("views", "views")
+app.set("view engine", "ejs")
+
 app.get("/", function(req, res) {
     res.send("welcome to the app")
 })
