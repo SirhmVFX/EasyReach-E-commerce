147f17dd8c6c0677c848d4cfc378f955cc7c01c0
sirhmvfx <sirhmvfx@gmail.com>
2023-04-13 09:22:44 +0100

    debugging

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 
+const router = require("./router")
+
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
@@ -8,8 +10,6 @@ app.use(express.static("public"))
 app.set("views", "views")
 app.set("view engine", "ejs")
 
-app.get("/", function(req, res) {
-    res.send("login")
-})
+app.use("/", router)
 
 app.listen(8000)  
