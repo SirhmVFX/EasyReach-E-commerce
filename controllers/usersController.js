dddcda4e061ef70f2212e5daa327d971f21077a1
sirhmvfx <sirhmvfx@gmail.com>
2023-04-18 15:57:20 +0100

    added home page

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,7 @@
+exports.homepage = function(req, res) {
+    res.render("homepage")
+}
+
 exports.login = function(req, res) {
     res.render("login", {pageTitle: "Sign In - EasyReach"})
 }
--- router.js
+++ router.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const usersController = require("./controllers/usersController")
 
+router.get("/", usersController.homepage)
 router.get("/login", usersController.login)
 router.get("/signup", usersController.signup)
 
