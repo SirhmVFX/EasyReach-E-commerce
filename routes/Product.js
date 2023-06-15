493222c2b1015731cc6df21e42a49eda89d6ca6d
Solomon Olatunji <realsolomon01@outlook.com>
2023-06-15 06:02:39 +0100

    update product

---
+++ routes/Product.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require('../middlewares/authentication');
+
+const {
+  createProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+  searchProducts,
+  getAllCategories,
+  searchCategoryProducts
+} = require('../controllers/productController');
+
+const { getSingleProductReviews } = require('../controllers/reviewController');
+
+router
+  .route('/')
+  .post([authenticateUser, authorizePermissions('admin')], createProduct)
+  .get(getAllProducts);
+
+
+router
+  .route('/find/:id')
+  .get(getSingleProduct)
+  .patch([authenticateUser, authorizePermissions('admin')], updateProduct)
+  .delete([authenticateUser, authorizePermissions('admin')], deleteProduct);
+
+router.route('/:id/reviews').get(getSingleProductReviews);
+router.route('/search').get(searchProducts);
+router.route('/category').get(searchCategoryProducts);
+router.route('/categories').get(getAllCategories);
+
+module.exports = router;
