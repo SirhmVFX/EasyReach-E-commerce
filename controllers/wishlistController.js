c7eae7be07e710bdeed5ddfef55afaae9f812374
Solomon Olatunji <realsolomon01@outlook.com>
2023-06-15 17:42:35 +0100

    update wishlist... controller tomorrow

---
+++ controllers/wishlistController.js
@@ -0,0 +1,4 @@
+const User = require("../models/User");
+const Wishlist = require("../models/Wishlist");
+const { StatusCodes } = require("http-status-codes");
+const HttpError = require("../HttpException");
